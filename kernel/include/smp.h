95f283cd41ab3eefc55d083c5cac91a83bad2b7b
too-r <william@huddie.net>
2018-05-04 08:41:47 +0100

    Added SMP support to run target and also added target for running QEMU with KVM enabled

--- kernel/include/smp.h
+++ kernel/include/smp.h
@@ -11,7 +11,7 @@
 })
 
 #define fsw(offset, value) ({ \
-    asm volatile ("mov qword ptr, fs:[rbx], rax", : : "a"(value), "b"(offset) :); \
+    asm volatile ("mov qword ptr fs:[rbx], rax", : : "a"(value), "b"(offset) :); \
 })
 #endif
 
