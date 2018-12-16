5846cd128d597ce09c6ae218bad06333b29c4405
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-12-16 17:40:46 +0100

    elf: Pass argv and environ to userspace

--- root/src/hello/hello.c
+++ root/src/hello/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+int main(int argc, char **argv) {
+    for (int i = 0; i < argc; i++)
+        printf("My %d-th argument is %s\n", i, argv[i]);
+    printf("getenv(\"FOO\") returns %s\n", getenv("FOO"));
     for (;;) {
         char prompt[256];
         fprintf(stdout, "qword> ");
