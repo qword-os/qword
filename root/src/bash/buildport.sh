f71d6d0a15f210f1e2b36b823d83b3ad1942dab9
mintsuki <mintsuki@protonmail.com>
2019-01-06 02:33:10 +0100

    Update bash port to 4.4.23, add make port

--- root/src/bash/buildport.sh
+++ root/src/bash/buildport.sh
@@ -3,7 +3,7 @@
 set -e
 
 PKG_NAME=bash
-PKG_VERSION=4.4.18
+PKG_VERSION=4.4
 PKG_URL=https://ftp.gnu.org/gnu/bash/bash-$PKG_VERSION.tar.gz
 PKG_TARBALL=$PKG_NAME-$PKG_VERSION.tar.gz
 PKG_ARCHIVE_DIR=$PKG_NAME-$PKG_VERSION
@@ -20,12 +20,34 @@ set -x
 
 rm -rf $PKG_ARCHIVE_DIR
 if [ ! -f $PKG_TARBALL ]; then
-	wget $PKG_URL
+    wget $PKG_URL
 fi
+
+# bash-specific: get all the patches
+BASH_PATCHES="
+001 002 003 004 005 006 007 008 009 010
+011 012 013 014 015 016 017 018 019 020
+021 022 023
+"
+
+# XXX update this in the future
+BASH_PATCH_URL_BASE="https://ftp.gnu.org/gnu/bash/bash-4.4-patches/bash44"
+BASH_PATCH_BASENAME="bash44"
+
+for i in $BASH_PATCHES; do
+    if [ ! -f $BASH_PATCH_BASENAME-$i ]; then
+        wget $BASH_PATCH_URL_BASE-$i
+    fi
+done
 
 tar -xf $PKG_TARBALL
 cd $PKG_ARCHIVE_DIR
 patch -p1 < ../$PKG_NAME-$PKG_VERSION.patch
+
+# bash-specific: apply all patches
+for i in $BASH_PATCHES; do
+    patch -p0 < ../$BASH_PATCH_BASENAME-$i
+done
 
 ./configure --host=x86_64-qword --prefix=$PKG_PREFIX --without-bash-malloc
 make "$@"
