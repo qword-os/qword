9292fd1728170df2928fce4fa897a8e9819e9937
mintsuki <mintsuki@protonmail.com>
2019-02-02 20:21:40 +0100

    Sort in makeworld.sh

--- root/src/makeworld.sh
+++ root/src/makeworld.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-BUILDPORTS=$(find ./ -type f -name 'buildport.sh' -printf '%h\n')
+BUILDPORTS=$(find ./ -type f -name 'buildport.sh' -printf '%h\n' | sort)
 for i in $BUILDPORTS ; do pushd $i && ./buildport.sh "$@" && popd ; done
