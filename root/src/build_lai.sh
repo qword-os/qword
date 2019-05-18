34073c0f8e5edce5a3b59c87c33ee8e24c2e1f7f
too-r <william@huddie.net>
2019-05-18 17:34:33 +0100

    Remove pointless use of meson in lai build script and fix kernel makefile

--- root/src/build_lai.sh
+++ root/src/build_lai.sh
@@ -3,13 +3,5 @@ set -e
 PKG_URL=https://github.com/windozz/lai.git
 
 git clone $PKG_URL
-cd lai
-mkdir build && cd build
-meson ..
-ninja
-pwd
-cp liblai.a ../../kernel/
-cd ../..
-pwd
 cp -r lai/ ./kernel/acpi/
 rm -rf lai
