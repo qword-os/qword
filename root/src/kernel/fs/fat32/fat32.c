4db5fec5036f3f13551aa7a3f0e13578adb41fb9
qookie <kacper.slominski72@gmail.com>
2019-05-23 19:05:32 +0200

    Added a few FAT32 fs functions and buggy read support

--- root/src/kernel/fs/fat32/fat32.c
+++ root/src/kernel/fs/fat32/fat32.c
@@ -10,6 +10,8 @@
 #define SECTOR_TO_OFF(x) ((x) << SECTOR_SHIFT)
 #define CLUSTER_TO_OFF(x, c, s) (SECTOR_TO_OFF((c) + ((x) - 2) * (s)))
 
+#define PATH_MAX 254
+
 static void read_off(int handle, uint64_t location, void *dst, size_t len) {
     lseek(handle, location, SEEK_SET);
     read(handle, dst, len);
@@ -35,16 +37,34 @@ struct fs_ent {
 };
 
 struct mount_t {
+    int device;
+    struct fs_info info;
 };
 
+struct handle_t {
+    int free;
+    int refcount;
+    int mount;
+    int flags;
+    char path[PATH_MAX];
+    struct fs_ent ent;
+    size_t offset;
+};
+
+static struct mount_t *mounts = NULL;
 static int mount_i = 0;
 
-static int read_ent(int device, struct fs_info info, uint64_t cluster, size_t index, struct fs_ent *dest) {
-    uint64_t off = CLUSTER_TO_OFF(cluster, info.cluster_begin_off, info.sectors_per_cluster) + index * 32;
+static struct handle_t *handles = NULL;
+static int handle_i = 0;
+
+static int read_ent(struct mount_t *mnt, uint32_t cluster,
+                    size_t index, struct fs_ent *dest) {
+    uint64_t off = CLUSTER_TO_OFF(cluster, mnt->info.cluster_begin_off, 
+                                    mnt->info.sectors_per_cluster) + index * 32;
 
     uint8_t buf[32];
     memset(buf, 0, 32);
-    read_off(device, off, buf, 32);
+    read_off(mnt->device, off, buf, 32);
 
     if (!*buf) return 0;
 
@@ -58,8 +78,19 @@ static int read_ent(int device, struct fs_info info, uint64_t cluster, size_t in
     return 1;
 }
 
+static inline uint32_t next_cluster(uint32_t cluster, uint32_t *fat, uint32_t fat_len) {
+    if (cluster >= fat_len / 4) return 0;
+    if (fat[cluster] >= 0xFFFFFFF8) return 0;
+    return fat[cluster];
+}
+
 static int fat32_mount(const char *source) {
     int device = open(source, O_RDONLY);
+
+    if (device < 0) {
+        kprint(KPRN_ERR, "fat32: failed to open mount source!");
+        return -1;
+    }
 
     kprint(KPRN_INFO, "fat32: mounting");
 
@@ -73,38 +104,205 @@ static int fat32_mount(const char *source) {
 
     info.fat_off = info.reserved_sectors;
     info.cluster_begin_off = (info.reserved_sectors + info.num_fats * info.sectors_per_fat);
+    
+    mounts = krealloc(mounts, sizeof(struct mount_t) * (mount_i + 1));
+    
+    struct mount_t *mnt = &mounts[mount_i];
+   
+    mnt->device = device;
+    mnt->info = info;
 
-    kprint(KPRN_DBG, "fat32: b/s: %u", info.bytes_per_sector);
-    kprint(KPRN_DBG, "fat32: s/c: %u", info.sectors_per_cluster);
-    kprint(KPRN_DBG, "fat32: reserved: %u", info.reserved_sectors);
-    kprint(KPRN_DBG, "fat32: fats: %u", info.num_fats);
-    kprint(KPRN_DBG, "fat32: s/f: %u", info.sectors_per_fat);
-    kprint(KPRN_DBG, "fat32: root: %u", info.root_dir_cluster);
-    kprint(KPRN_DBG, "fat32: offset to fat(sectors): %u", info.fat_off);
-    kprint(KPRN_DBG, "fat32: offset to first cluster(sectors): %u", info.cluster_begin_off);
+    return mount_i++;
+}
 
-    struct fs_ent ent;
+#define min(a, b) ((a) > (b) ? (b) : (a))
+#define islower(c) ((c) >= 'a' && (c) <= 'z')
 
-    for (size_t i = 0; read_ent(device, info, info.root_dir_cluster, i, &ent); i++) {
-        if (ent.attrib == 0x0F) continue; // skip lfn
+int toupper(int c) {
+    if (islower(c)) return c - ('a' - 'A');
+    else return c;
+}
+
+// TODO: actually parse lfn and only resort to this when there's no lfn
+static int compare_fs_ent_and_path(struct fs_ent *ent, const char *path) {
+    const char *ext = strchrnul(path, '.');
+    
+    char name[12];
+    memset(name, 0, 12);
+    memset(name, ' ', 11);
+    if (*(ext - 1) == '.') {
+        memcpy(name, path, min(ext - path - 1, 8));
+        memcpy(name + 8, ext, min(strlen(ext), 3));
+    } else {
+        memcpy(name, path, min(strlen(path), 8));
+    }
+
+    for (size_t i = 0; i < 11; i++) name[i] = toupper(name[i]);
+
+    return memcmp(ent->name, name, 11);
+}
+
+static struct fs_ent parse_path(int mount, const char *path) {
+    struct mount_t *mnt = &mounts[mount];
+
+    struct fs_ent root = {
+        .name = {' '},
+        .file_size = 0,
+        .begin_cluster = mnt->info.root_dir_cluster,
+    };
+    if (path[0] == '/' && path[1] == '\0') return root;
+    
+    uint32_t cluster = mnt->info.root_dir_cluster;
+   
+    path++;
+
+    size_t fat_len = mnt->info.sectors_per_fat * 512;
+    uint32_t *fat = kalloc(fat_len);
+    read_off(mnt->device, SECTOR_TO_OFF(mnt->info.fat_off), fat, fat_len);
+ 
+    struct fs_ent ent = {0};
+    for (size_t i = 0; read_ent(mnt, cluster, i, &ent); i++) {
+        if (i == mnt->info.sectors_per_cluster * 16) {
+            i = 0;
+            cluster = next_cluster(cluster, fat, fat_len);
+        }
+
+        if ((ent.attrib & 0x0F) == 0x0F) continue; // skip lfn for now
         if (ent.name[0] == 0xE5) continue;
         if (ent.name[0] == 0x05) ent.name[0] = 0xE5;
 
-        char c[12];
-        memset(c, 0, 12);
-        memcpy(c, ent.name, 11);
-        kprint(KPRN_DBG, "fat32: '%s' starts on %u cluster, is %u bytes long, attrib %x", c, ent.begin_cluster, ent.file_size, ent.attrib);
-    
-        uint64_t off = CLUSTER_TO_OFF(ent.begin_cluster, info.cluster_begin_off, info.sectors_per_cluster);
+        char sub_elem[PATH_MAX] = {0};
+        int top_level = 0;
 
-        uint8_t buf[info.sectors_per_cluster * info.bytes_per_sector];
-        read_off(device, off, buf, info.sectors_per_cluster * info.bytes_per_sector);
-        buf[511] = 0;
+        const char *slash = strchrnul(path, '/');
+        if (*slash == '\0') {
+            memcpy(sub_elem, path, strlen(path));
+            top_level = 1;
+        } else {
+            memcpy(sub_elem, path, slash - path);
+            sub_elem[slash - path] = '\0';
+        }
 
-        kprint(KPRN_DBG, "fat32: '%s' contents: '%s'", c, buf);
+        if (!compare_fs_ent_and_path(&ent, sub_elem)) {
+
+            if (top_level) {
+                // found file we were looking for
+                kfree(fat);
+                return ent;
+            } else {
+                cluster = ent.begin_cluster;
+                i = 0;
+            }
+        }
     }
 
-    return mount_i++;
+    kfree(fat);
+    struct fs_ent err = {0};
+    return err;
+}
+
+static int create_handle(struct handle_t handle) {
+    for (int i = 0; i < handle_i; i++) {
+        if (handles[i].free) {
+            handles[i] = handle;
+            return i;
+        }
+    }
+    int handle_n = 0;
+    handles = krealloc(handles, (handle_i + 1) * sizeof(struct handle_t));
+    handle_n = handle_i++;
+    handles[handle_n] = handle;
+    return handle_n;
+}
+
+static int fat32_open(const char *path, int flags, int mount) {
+    struct handle_t handle = {0};
+    strcpy(handle.path, path);
+    handle.flags = flags;
+    handle.mount = mount;
+    handle.refcount = 1;
+    handle.free = 0;
+    handle.offset = 0;
+
+    handle.ent = parse_path(mount, path);
+    return create_handle(handle);
+}
+
+static int fat32_read(int handle, void *buf, size_t count) {
+    if (handle >= handle_i) return -1;
+    if (handles[handle].free) return -1;
+    struct mount_t *mnt = &mounts[handles[handle].mount];
+    struct fs_ent *ent = &handles[handle].ent;
+
+    if (handles[handle].offset >= ent->file_size) return -1;
+
+    size_t fat_len = mnt->info.sectors_per_fat * 512;
+    uint32_t *fat = kalloc(fat_len);
+    read_off(mnt->device, SECTOR_TO_OFF(mnt->info.fat_off), fat, fat_len);
+
+    kprint(KPRN_DBG, "fat32: in read");
+ 
+    int read_size = min(count, ent->file_size);
+
+    size_t bytes_per_cluster = mnt->info.sectors_per_cluster * 512;
+    size_t cluster_bytes = ((read_size + bytes_per_cluster - 1) / bytes_per_cluster) * bytes_per_cluster;
+    
+    size_t cluster_offset = (handles[handle].offset + bytes_per_cluster - 1) / bytes_per_cluster;
+    size_t cluster = ent->begin_cluster + cluster_offset;
+    while (cluster_offset--) cluster = next_cluster(cluster, fat, fat_len);
+    size_t index = 0;
+
+    while (index + handles[handle].offset < cluster_bytes) {
+        size_t off = CLUSTER_TO_OFF(cluster, mnt->info.cluster_begin_off, 
+                                    mnt->info.sectors_per_cluster);
+        read_off(mnt->device, off, (buf + index), min(cluster_bytes - index, bytes_per_cluster));
+        index += bytes_per_cluster;
+        
+        cluster = next_cluster(cluster, fat, fat_len);
+    }
+
+    kfree(fat);
+    handles[handle].offset += read_size;
+
+    return read_size;
+}
+
+static int fat32_write(int handle, const void *buf, size_t count) {
+    return -1;      // R/O for now
+}
+
+static int fat32_close(int handle) {
+    if (handle >= handle_i) return -1;
+    if (handles[handle].free) return -1;
+    if (!(--handles[handle].refcount))
+        handles[handle].free = 1;
+
+    return 0;
+}
+
+static int fat32_dup(int handle) {
+    if (handle < 0) return -1;
+    if (handle >= handle_i) return -1;
+    if (handles[handle].free) return -1;
+    handles[handle].refcount++;
+
+    return 0;
+}
+
+static int fat32_fstat(int handle, struct stat *st) {
+    if (handle < 0) return -1;
+    if (handle >= handle_i) return -1;
+    if (handles[handle].free) return -1;
+
+    return 0;
+}
+
+static int fat32_readdir(int handle, struct dirent *d) {
+    return 0;
+}
+
+static int fat32_sync() {
+    return 0;
 }
 
 void init_fs_fat32(void) {
@@ -113,15 +311,15 @@ void init_fs_fat32(void) {
     fat32 = default_fs_handler;
     strcpy(fat32.name, "fat32");
     fat32.mount = (void *)fat32_mount;
-    /*fat32.open = fat32_open;
-    fat32.read = fat32_read;
-    fat32.lseek = fat32_seek;
-    fat32.fstat = fat32_fstat;
+    fat32.open = fat32_open;
     fat32.close = fat32_close;
     fat32.dup = fat32_dup;
-    fat32.write = fat32_write;
+    fat32.read = fat32_read;
     fat32.readdir = fat32_readdir;
-    fat32.sync = fat32_sync;*/
+    fat32.fstat = fat32_fstat;
+    fat32.write = fat32_write;
+    /*fat32.lseek = fat32_seek;*/
+    fat32.sync = fat32_sync;
 
     vfs_install_fs(&fat32);
 }
