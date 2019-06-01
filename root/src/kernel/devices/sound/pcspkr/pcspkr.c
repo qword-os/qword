6a7534350bf3ed7032e838dca434aff97b443318
too-r <william@huddie.net>
2019-06-01 16:49:55 +0100

    Use a consistent coding style for pcspkr.c

--- root/src/kernel/devices/sound/pcspkr/pcspkr.c
+++ root/src/kernel/devices/sound/pcspkr/pcspkr.c
@@ -2,36 +2,33 @@
 #include <lib/cio.h>
 #include <lib/time.h>
 
-//Play sound using built in speaker
-static void play_sound(uint32_t nFrequence) {
-    uint32_t Div;
-    uint8_t tmp;
-
-    //Set the PIT to the desired frequency
-    Div = 1193180 / nFrequence;
+// Play sound using built in speaker
+static void play_sound(uint32_t frequency) {
+    // Set the PIT to the desired frequency
+    uint32_t x = 1193180 / frequency;
     port_out_b(0x43, 0xb6);
-    port_out_b(0x42, (uint8_t) (Div) );
-    port_out_b(0x42, (uint8_t) (Div >> 8));
+    port_out_b(0x42, (uint8_t) (x) );
+    port_out_b(0x42, (uint8_t) (x >> 8));
 
-    //And play the sound using the PC speaker
-    tmp = port_in_b(0x61);
+    // And play the sound using the PC speaker
+    uint32_t tmp = port_in_b(0x61);
 
     if (tmp != (tmp | 3)) {
         port_out_b(0x61, tmp | 3);
     }
 }
 
-//make it shutup
-static void nosound(void) {
+// Stop playback of sound
+static void stop(void) {
     uint8_t tmp = port_in_b(0x61) & 0xFC;
 
     port_out_b(0x61, tmp);
 }
 
-//Make a beep
+// Make a beep
 void beep(void) {
- 	play_sound(1000);
- 	ksleep(100);
- 	nosound();
+    play_sound(1000);
+    ksleep(100);
+    stop();
     //set_PIT_2(old_frequency);
 }
