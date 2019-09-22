6ff60c99d472d6269ef48dc46031c3182ffdebf0
mintsuki <mintsuki@protonmail.com>
2019-09-22 04:20:12 +0200

    Sort usable RAM entries in e820

--- sys/e820.c
+++ sys/e820.c
@@ -8,6 +8,7 @@ void get_e820(void *);
 uint64_t memory_size = 0;
 
 struct e820_entry_t e820_map[256];
+static struct e820_entry_t e820_map_unsorted[256];
 
 static const char *e820_type(uint32_t type) {
     switch (type) {
@@ -28,7 +29,33 @@ static const char *e820_type(uint32_t type) {
 
 void init_e820(void) {
     /* Get e820 memory map. */
-    get_e820(e820_map);
+    get_e820(e820_map_unsorted);
+
+    /* Sort the entries: free RAM first, anything else after */
+    size_t usable_ram_entry_count = 0;
+    size_t j = 0;
+    for (size_t i = 0; e820_map_unsorted[i].type; i++) {
+        if (e820_map_unsorted[i].type == 1) {
+            usable_ram_entry_count++;
+            e820_map[j++] = e820_map_unsorted[i];
+        }
+    }
+    for (size_t i = 0; e820_map_unsorted[i].type; i++) {
+        if (e820_map_unsorted[i].type != 1)
+            e820_map[j++] = e820_map_unsorted[i];
+    }
+
+    /* Bubble sort usable RAM entries by size of base */
+    for (size_t i = 0; i < usable_ram_entry_count - 1; i++) {
+        size_t t = usable_ram_entry_count - i - 1;
+        for (size_t j = 0; j < t; j++) {
+            if (e820_map[j].base > e820_map[j+1].base) {
+                struct e820_entry_t e = e820_map[j];
+                e820_map[j] = e820_map[j+1];
+                e820_map[j+1] = e;
+            }
+        }
+    }
 
     /* Print out memory map and find total usable memory. */
     for (size_t i = 0; e820_map[i].type; i++) {
