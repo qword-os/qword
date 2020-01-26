c83a379007e24a7b2079081f1209c7ef88ef593f
Itay Almog <itay2828@gmail.com>
2020-01-26 21:52:15 +0200

    GPT support

--- lib/part.c
+++ lib/part.c
@@ -86,10 +86,120 @@ static int create_partition_device(const char *device, int part_no, uint64_t fir
     return 0;
 }
 
+struct gpt_table_header_t {
+    // the head
+    uint64_t signature;
+    uint32_t revision;
+    uint32_t header_size;
+    uint32_t crc32;
+    uint32_t _reserved0;
+
+    // the partitioning info
+    uint64_t my_lba;
+    uint64_t alternate_lba;
+    uint64_t first_usable_lba;
+    uint64_t last_usable_lba;
+
+    // the guid
+    struct {
+        uint64_t low;
+        uint64_t high;
+    } disk_guid;
+
+    // entries related
+    uint64_t partition_entry_lba;
+    uint32_t number_of_partition_entries;
+    uint32_t size_of_partition_entry;
+    uint32_t partition_entry_array_crc32;
+} __attribute__((packed));
+
+#define GPT_ATTR_IMPORTANT (1u << 0u)
+#define GPT_ATTR_DONT_MOUNT (1u << 1u)
+#define GPT_ATTR_FOR_LEGACY (1u << 2u)
+
+struct gpt_entry_t {
+    struct {
+        uint64_t low;
+        uint64_t high;
+    } partition_type_guid;
+
+    struct {
+        uint64_t low;
+        uint64_t high;
+    } unique_partition_guid;
+
+    uint64_t starting_lba;
+    uint64_t ending_lba;
+
+    uint64_t attributes;
+
+    uint16_t partition_name[36];
+} __attribute__((packed));
+
 
 static int enum_gpt(int fd, const char *device, struct device_t *dev_struct) {
+    struct gpt_table_header_t header = {0};
 
-    return -1;
+    kprint(KPRN_DBG, "trying to parse GPT on device /dev/%s", device);
+
+    // read header, located after the first block
+    lseek(fd, 512, SEEK_SET);
+    read(fd, &header, sizeof(header));
+
+    // check the header
+    // 'EFI PART'
+    if (header.signature != 'TRAP IFE') return -1;
+    if (header.revision != 0x00010000) return -1;
+    if (header.header_size < 92) return -1;
+    if (header.header_size > dev_struct->size) return -1;
+    if (header._reserved0 != 0) return -1;
+    // TODO: CRC32 check
+
+    // sanity check header values
+    if (header.my_lba != 1) return -1;
+    if (header.first_usable_lba > header.last_usable_lba) return -1;
+    if (header.partition_entry_lba >= header.first_usable_lba && header.partition_entry_lba <= header.last_usable_lba) return -1;
+    if (header.size_of_partition_entry % 128 != 0) return -1;
+    if (header.size_of_partition_entry != sizeof(struct gpt_entry_t)) return -1;
+    if (header.number_of_partition_entries > UINTPTR_MAX / header.size_of_partition_entry) return -1;
+    if ((header.number_of_partition_entries * header.size_of_partition_entry) % 512 != 0) return -1;
+    if (header.partition_entry_lba + 1 > header.first_usable_lba) return -1;
+
+    // parse the entries if reached here
+    struct gpt_entry_t entry = {0};
+    lseek(fd, header.partition_entry_lba * 512, SEEK_SET);
+    for (size_t i = 0; i < header.number_of_partition_entries; i++) {
+        read(fd, &entry, sizeof(entry));
+
+        // not an actual entry
+        if (entry.unique_partition_guid.low == 0 &&
+                entry.unique_partition_guid.high == 0) {
+            continue;
+        }
+
+        // skip the partition if has these attributes
+        if (entry.attributes & (GPT_ATTR_DONT_MOUNT | GPT_ATTR_FOR_LEGACY)) {
+            continue;
+        }
+
+        // turn the name
+        char name[36] = {0};
+        for (int j = 0; j < 36; j++) {
+            // display unicode as '?'
+            if (entry.partition_name[j] <= 0xFFu) {
+                name[j] = entry.partition_name[j] & 0xFFu;
+            } else {
+                name[j] = '?';
+            }
+        }
+
+        kprint(KPRN_DBG, "partition %s", name);
+        kprint(KPRN_DBG, "\tfirst_sect: %d", i, entry.starting_lba);
+        kprint(KPRN_DBG, "\tsect_count: %d", i, entry.ending_lba - entry.starting_lba);
+        create_partition_device(device, i, entry.starting_lba, entry.ending_lba - entry.starting_lba, dev_struct);
+    }
+
+    return 0;
 }
 
 struct mbr_entry {
