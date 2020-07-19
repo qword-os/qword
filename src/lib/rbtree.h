031d43dc93593049658b890184ff529a2b9dc13a
YayOrg <umz2003@gmail.com>
2020-07-19 15:56:46 +0300

    formatted code

--- src/lib/rbtree.h
+++ src/lib/rbtree.h
@@ -1,85 +1,84 @@
 #ifndef __RB_TREE_H__
 #define __RB_TREE_H__
 
-#include <stdint.h>
-#include <stddef.h>
 #include <lib/alloc.h>
+#include <stddef.h>
+#include <stdint.h>
 
-enum rb_color_t {
-    RB_COLOR_BLACK,
-    RB_COLOR_RED
-};
- 
+enum rb_color_t { RB_COLOR_BLACK, RB_COLOR_RED };
+
 struct rb_node {
     struct rb_node *desc[2];
     struct rb_node *anc;
     enum rb_color_t color;
 };
- 
-typedef int (*rb_comp)(struct rb_node*, struct rb_node*, void*);
- 
+
+typedef int (*rb_comp)(struct rb_node *, struct rb_node *, void *);
+
 struct rb_root_t {
     struct rb_node *root;
     size_t node_size;
 };
- 
+
 static inline struct rb_node *rb_get_par(struct rb_node *node) {
     if (node == NULL) {
         return NULL;
     }
     return node->anc;
 }
- 
+
 static inline void rb_set_par(struct rb_node *node, struct rb_node *par) {
     if (node != NULL) {
         node->anc = par;
     }
 }
- 
+
 static inline struct rb_node *rb_get_desc(struct rb_node *node, int pos) {
     if (node == NULL) {
         return NULL;
     }
     return node->desc[pos];
 }
- 
-static inline void rb_set_desc(struct rb_node *node, int pos, struct rb_node *desc) {
+
+static inline void rb_set_desc(struct rb_node *node, int pos,
+                               struct rb_node *desc) {
     if (node != NULL) {
         node->desc[pos] = desc;
         rb_set_par(rb_get_desc(node, pos), node);
     }
 }
- 
+
 static inline struct rb_node *rb_get_gpar(struct rb_node *node) {
     return rb_get_par(rb_get_par(node));
 }
- 
+
 static inline enum rb_color_t rb_get_color(struct rb_node *node) {
     if (node == NULL) {
         return RB_COLOR_BLACK;
     }
     return node->color;
 }
- 
+
 static inline void rb_set_color(struct rb_node *node, enum rb_color_t color) {
     if (node != NULL) {
         node->color = color;
     }
 }
- 
+
 static inline int rb_get_pos(struct rb_node *node) {
     return rb_get_desc(rb_get_par(node), 1) == node;
 }
- 
+
 static inline struct rb_node *rb_get_sib(struct rb_node *node) {
     return rb_get_desc(rb_get_par(node), 1 - rb_get_pos(node));
 }
- 
+
 static inline struct rb_node *rb_get_unc(struct rb_node *node) {
     return rb_get_sib(rb_get_par(node));
 }
- 
-static inline void *rb_rotate(struct rb_node *root, int direction, struct rb_root_t* tree) {
+
+static inline void *rb_rotate(struct rb_node *root, int direction,
+                              struct rb_root_t *tree) {
     int root_pos = rb_get_pos(root);
     struct rb_node *root_parent = rb_get_par(root);
     struct rb_node *new_root = rb_get_desc(root, 1 - direction);
@@ -94,8 +93,9 @@ static inline void *rb_rotate(struct rb_node *root, int direction, struct rb_roo
         rb_set_par(new_root, NULL);
     }
 }
- 
-static inline void rb_fix_insertion(struct rb_node* node, struct rb_root_t* root) {
+
+static inline void rb_fix_insertion(struct rb_node *node,
+                                    struct rb_root_t *root) {
     if (node == NULL) {
         return;
     }
@@ -109,12 +109,12 @@ static inline void rb_fix_insertion(struct rb_node* node, struct rb_root_t* root
     if (rb_get_color(uncle) == RB_COLOR_BLACK) {
         // black uncle
         if (rb_get_pos(par) == node_pos) {
-            //line
+            // line
             rb_rotate(gpar, 1 - node_pos, root);
             rb_set_color(gpar, RB_COLOR_RED);
             rb_set_color(par, RB_COLOR_BLACK);
         } else {
-            //triangle
+            // triangle
             rb_rotate(par, 1 - node_pos, root);
             rb_rotate(gpar, node_pos, root);
             rb_set_color(gpar, RB_COLOR_RED);
@@ -128,9 +128,10 @@ static inline void rb_fix_insertion(struct rb_node* node, struct rb_root_t* root
         rb_fix_insertion(gpar, root);
     }
 }
- 
+
 // 0 - node is added already, 1 - new node added
-static inline int rb_insert(struct rb_root_t* root, rb_comp comp, void* comp_arg, struct rb_node* node) {
+static inline int rb_insert(struct rb_root_t *root, rb_comp comp,
+                            void *comp_arg, struct rb_node *node) {
     rb_set_desc(node, 0, NULL);
     rb_set_desc(node, 1, NULL);
     if (root->root == NULL) {
@@ -157,23 +158,21 @@ static inline int rb_insert(struct rb_root_t* root, rb_comp comp, void* comp_arg
     rb_set_color(root->root, RB_COLOR_BLACK);
     return 1;
 }
- 
- 
- 
+
 static inline struct rb_node *rb_find_replacement(struct rb_node *node) {
     node = rb_get_desc(node, 0);
-    struct rb_node* prev = node;
+    struct rb_node *prev = node;
     while (node != NULL) {
         prev = node;
         node = rb_get_desc(node, 1);
     }
     return prev;
 }
- 
-static inline void rb_memswap(char* p1, char* p2, size_t size) {
+
+static inline void rb_memswap(char *p1, char *p2, size_t size) {
     if (size % 8 == 0) {
-        uint64_t* pl1 = (uint64_t*)p1;
-        uint64_t* pl2 = (uint64_t*)p2;
+        uint64_t *pl1 = (uint64_t *)p1;
+        uint64_t *pl2 = (uint64_t *)p2;
         size_t count = size / 8;
         for (size_t i = 0; i < count; ++i) {
             uint64_t tmp = *pl1;
@@ -188,24 +187,26 @@ static inline void rb_memswap(char* p1, char* p2, size_t size) {
         }
     }
 }
- 
-static inline void rb_swap_nodes_content(struct rb_node *node1, struct rb_node *node2, struct rb_root_t *root) {
-    char* content1 = (char*)(node1 + 1);
-    char* content2 = (char*)(node2 + 1);
+
+static inline void rb_swap_nodes_content(struct rb_node *node1,
+                                         struct rb_node *node2,
+                                         struct rb_root_t *root) {
+    char *content1 = (char *)(node1 + 1);
+    char *content2 = (char *)(node2 + 1);
     size_t size = root->node_size - sizeof(struct rb_node);
     rb_memswap(content1, content2, size);
 }
- 
-static struct rb_node* rb_remove_internal_nodes(struct rb_root_t *root, struct rb_node *node) {
+
+static struct rb_node *rb_remove_internal_nodes(struct rb_root_t *root,
+                                                struct rb_node *node) {
     if (rb_get_desc(node, 0) != NULL && rb_get_desc(node, 1) != NULL) {
-        struct rb_node* next = rb_find_replacement(node);
+        struct rb_node *next = rb_find_replacement(node);
         rb_swap_nodes_content(next, node, root);
         return next;
     }
     return node;
 }
- 
- 
+
 static void rb_fix_double_black(struct rb_root_t *root, struct rb_node *node) {
     if (rb_get_par(node) == NULL) {
         return;
@@ -222,7 +223,8 @@ static void rb_fix_double_black(struct rb_root_t *root, struct rb_node *node) {
     }
     // black sibling case
     // if sibling's child are all black
-    if (rb_get_color(rb_get_desc(sib, 0)) == RB_COLOR_BLACK && rb_get_color(rb_get_desc(sib, 1)) == RB_COLOR_BLACK) {
+    if (rb_get_color(rb_get_desc(sib, 0)) == RB_COLOR_BLACK &&
+        rb_get_color(rb_get_desc(sib, 1)) == RB_COLOR_BLACK) {
         if (rb_get_color(par) == RB_COLOR_RED) {
             rb_set_color(par, RB_COLOR_BLACK);
             rb_set_color(sib, RB_COLOR_RED);
@@ -247,7 +249,7 @@ static void rb_fix_double_black(struct rb_root_t *root, struct rb_node *node) {
         rb_set_color(red_nephew, RB_COLOR_BLACK);
     }
 }
- 
+
 // 0 - there is no such node, 1 - node deleted
 static inline void rb_delete(struct rb_root_t *root, struct rb_node *node) {
     // handle case with internal node
@@ -275,8 +277,10 @@ static inline void rb_delete(struct rb_root_t *root, struct rb_node *node) {
     rb_set_desc(rb_get_par(node), rb_get_pos(node), NULL);
     kfree(node);
 }
- 
-static inline struct rb_node *rb_query(struct rb_root_t *root, struct rb_node* node, rb_comp comp, void* comp_arg) {
+
+static inline struct rb_node *rb_query(struct rb_root_t *root,
+                                       struct rb_node *node, rb_comp comp,
+                                       void *comp_arg) {
     struct rb_node *current = root->root;
     while (current != NULL) {
         int cmp = comp(node, current, comp_arg);
