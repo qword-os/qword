e90c9c210ccf3b67f8a5c08e0d184cceab870aa3
mintsuki <mintsuki@protonmail.com>
2020-09-16 12:38:51 +0200

    Do not use hardcoded bootstrap stack address

--- src/startup/header.asm
+++ src/startup/header.asm
@@ -3,9 +3,16 @@ section .stivalehdr
 align 4
 
 stivale_header:
-    .stack: dq 0xeffff0  ; Stack pointer.
-    .videomode: dw 1     ; VESA.
-    .fbwidth: dw 0       ; Framebuffer info: 0 for default.
-    .fbheight: dw 0      ; Ditto.
-    .fbbpp: dw 0         ; Ditto.
+    .stack: dq entry_stack.top ; Stack pointer.
+    .videomode: dw 1           ; VESA.
+    .fbwidth: dw 0             ; Framebuffer info: 0 for default.
+    .fbheight: dw 0            ; Ditto.
+    .fbbpp: dw 0               ; Ditto.
     .entry: dq 0
+
+section .bss
+
+align 16
+entry_stack:
+    resb 8192
+  .top:
